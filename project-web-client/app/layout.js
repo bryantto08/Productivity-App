854d14712fab10a31abb5de9265dd3822b55273b
Bryant To <bryantto08@gmail.com>
2023-11-10 11:08:09 -0500

    adding cookies

--- project-web-client/app/layout.js
+++ project-web-client/app/layout.js
@@ -1,17 +1,21 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from './navbar/navbar.js'
+import { cookies } from 'next/headers'
+import { ClientCookiesProvider } from './cookie-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
 
 export default function RootLayout({ children }) {
   return (
+    <ClientCookiesProvider value={cookies().getAll()}>
     <html lang="en">
       <body className={inter.className}>
         <Navbar/>
         {children}
       </body>
     </html>
+    </ClientCookiesProvider>
   )
 }
