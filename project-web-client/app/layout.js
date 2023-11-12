eb857c2b9760bc8b863b5714da1be1418a998b39
Bryant To <bryantto08@gmail.com>
2023-11-12 12:39:20 -0500

    https connection

--- project-web-client/app/layout.js
+++ project-web-client/app/layout.js
@@ -1,11 +1,14 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import https from 'https'
+import axios from 'axios'
 import Navbar from './navbar/navbar.js'
 import { cookies } from 'next/headers'
 import { ClientCookiesProvider } from './cookie-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 
 export default function RootLayout({ children }) {
   return (
